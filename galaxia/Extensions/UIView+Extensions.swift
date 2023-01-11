60d915ca9d4f1505b26da2d2b83d83147cdb7407
jaseel <jazeelop5008@gmail.com>
2023-01-11 16:04:31 +0530

    initial project setup

---
+++ galaxia/Extensions/UIView+Extensions.swift
@@ -0,0 +1,93 @@
+//
+//  UIView+Extensions.swift
+//  galaxia
+//
+//  Created by Meridian Mac Mini on 11/01/23.
+//
+
+
+import UIKit
+
+@IBDesignable
+extension UIView {
+    
+    @IBInspectable
+    var cornerRadius: CGFloat {
+        get {
+            return self.cornerRadius
+        }
+        set {
+            let cornerRadius = newValue
+            
+            if UIDevice.current.userInterfaceIdiom == .phone {
+                layer.cornerRadius = cornerRadius
+            } else {
+                layer.cornerRadius = cornerRadius + 5
+            }
+            
+        }
+    }
+    
+    @IBInspectable var cornerDivided: CGFloat {
+        get {
+            return self.cornerDivided
+        }
+        set {
+            DispatchQueue.main.async {
+                let cornerRadius = self.frame.size.height / newValue
+                self.layer.cornerRadius = CGFloat(Double(cornerRadius))
+            }
+        }
+    }
+    
+    @IBInspectable
+    var shadowRadius: CGFloat {
+        get {
+            return layer.shadowRadius
+        }
+        set {
+            layer.shadowRadius = newValue
+        }
+    }
+    
+    @IBInspectable
+    var shadowOpacity: Float {
+        get {
+            return layer.shadowOpacity
+        }
+        set {
+            layer.shadowOpacity = newValue
+        }
+    }
+    
+    @IBInspectable
+    var shadowOffset: CGSize {
+        get {
+            return layer.shadowOffset
+        }
+        set {
+            layer.shadowOffset = newValue
+        }
+    }
+    
+    @IBInspectable
+    var shadowColor: UIColor? {
+        get {
+            if let color = layer.shadowColor {
+                return UIColor(cgColor: color)
+            }
+            return nil
+        }
+        set {
+            if let color = newValue {
+                layer.shadowColor = color.cgColor
+            } else {
+                layer.shadowColor = nil
+            }
+        }
+    }
+    
+    
+    
+
+}
---
+++ galaxia/Modules/Home/Cells/GalaxyCell/GalaxyCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  GalaxyCollectionViewCell.swift
+//  galaxia
+//
+//  Created by Meridian Mac Mini on 11/01/23.
+//
+
+import UIKit
+
+class GalaxyCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
