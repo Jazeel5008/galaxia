4d372a09f0ace6f649847c36e68a71a5d9123815
jaseel <jazeelop5008@gmail.com>
2023-01-11 16:26:57 +0530

    initial commit

--- galaxia/Modules/Home/Controller/HomeViewController.swift
+++ galaxia/Modules/Home/Controller/HomeViewController.swift
@@ -6,24 +6,18 @@
 //
 
 import UIKit
+import FSPagerView
 
 class HomeViewController: UIViewController {
 
+    @IBOutlet weak var pagerView: FSPagerView!
+    
+    @IBOutlet weak var cv_galaxies: UICollectionView!
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
